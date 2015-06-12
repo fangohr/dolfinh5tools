93cfc6f87648b23dc1c417afcacb21b333aee97b
mb4e10 <mb4e10@soton.ac.uk>
2015-06-12 14:29:18 +0100

    Add more tests for scalar fields.

--- src/savingdata_test.py
+++ src/savingdata_test.py
@@ -4,15 +4,15 @@ import json
 from collections import OrderedDict
 from savingdata import SavingData, LoadingData
 
-h5filename = 'file.h5'
-jsonfilename = 'file.json'
 mesh = df.UnitSquareMesh(10, 10)
-functionspace = df.VectorFunctionSpace(mesh, 'CG', 1, 3)
-f = df.Function(functionspace)
 t_array = np.linspace(0, 1e-9, 5)
 
+def test_save_vector_field_data():
+    h5filename = 'file_vector.h5'
+    jsonfilename = 'file_vector.json'
+    functionspace = df.VectorFunctionSpace(mesh, 'CG', 1, 3)
+    f = df.Function(functionspace)
 
-def test_save_data():
     sd = SavingData(h5filename, jsonfilename, functionspace)
 
     sd.save_mesh()
@@ -24,8 +24,30 @@ def test_save_data():
 
     sd.close()
 
+def test_save_scalar_field_data():
+    h5filename = 'file_scalar.h5'
+    jsonfilename = 'file_scalar.json'
+    functionspace = df.FunctionSpace(mesh, 'CG', 1)
+    f = df.Function(functionspace)
 
-def test_load_data():
+    sd = SavingData(h5filename, jsonfilename, functionspace)
+
+    sd.save_mesh()
+
+    for i in range(len(t_array)):
+        f.assign(df.Constant(t_array[i]))
+    
+        sd.save_field(f, 'f', t_array[i])
+
+    sd.close()
+
+
+def test_load_vector_data():
+    h5filename = 'file_vector.h5'
+    jsonfilename = 'file_vector.json'
+    functionspace = df.VectorFunctionSpace(mesh, 'CG', 1, 3)
+    f = df.Function(functionspace)
+
     ld = LoadingData(h5filename, jsonfilename)
 
     for t in t_array:
@@ -37,7 +59,25 @@ def test_load_data():
     ld.close()
 
 
+def test_load_scalar_data():
+    h5filename = 'file_scalar.h5'
+    jsonfilename = 'file_scalar.json'
+    functionspace = df.FunctionSpace(mesh, 'CG', 1)
+    f = df.Function(functionspace)
+
+    ld = LoadingData(h5filename, jsonfilename)
+
+    for t in t_array:
+        f_loaded = ld.load_field('f', t)
+
+        f.assign(df.Constant(t))
+        assert np.all(f.vector().array() == f_loaded.vector().array())
+
+    ld.close()
+
+
 def test_saved():
+    jsonfilename = 'file_scalar.json'
     with open(jsonfilename) as jsonfile:
         jsonData = json.load(jsonfile, object_pairs_hook=OrderedDict)
     jsonfile.close()
