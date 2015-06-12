3cd6d47dd0857cee7a109ea8a0bf54af81eb5c89
Hans Fangohr Lubuntu 14.04 virt MBA <fangohr@soton.ac.uk>
2015-06-12 14:21:57 +0100

    White space tidy up (pep8)

--- src/savingdata.py
+++ src/savingdata.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 
 import dolfin as df
 
+
 class SavingData(object):
     def __init__(self, h5filename, jsonfilename, functionspace):
         self.functionspace = functionspace
@@ -10,7 +11,7 @@ class SavingData(object):
         self.jsonfilename = jsonfilename
 
         self.h5file = df.HDF5File(df.mpi_comm_world(), self.h5filename, 'w')
-        
+
         self.field_index = 0
         self.t_array = []
 
@@ -24,7 +25,6 @@ class SavingData(object):
     def save_field(self, f, field_name, t):
         name = field_name + str(self.field_index)
         self.h5file.write(f, name)
-        
         self.t_array.append(t)
 
         if not self.fieldsDict.has_key(field_name):
@@ -33,9 +33,12 @@ class SavingData(object):
             self.fieldsDict[field_name]['metadata'] = {}
 
         self.fieldsDict[field_name]['data'][name] = t
-        self.fieldsDict[field_name]['metadata']['family'] = self.functionspace.ufl_element().family()
-        self.fieldsDict[field_name]['metadata']['degree'] = self.functionspace.ufl_element().degree()
-        self.fieldsDict[field_name]['metadata']['dim'] = self.functionspace.ufl_element().value_shape()[0]
+        self.fieldsDict[field_name]['metadata']['family'] = \
+            self.functionspace.ufl_element().family()
+        self.fieldsDict[field_name]['metadata']['degree'] = \
+            self.functionspace.ufl_element().degree()
+        self.fieldsDict[field_name]['metadata']['dim'] = \
+            self.functionspace.ufl_element().value_shape()[0]
 
         self.dump_metadata(self.jsonfilename, self.fieldsDict)
 
@@ -46,7 +49,6 @@ class SavingData(object):
         with open(filename, 'w') as jsonfile:
             json.dump(data, jsonfile, indent=True)
         jsonfile.close()
-
 
     def close(self):
         self.h5file.close()
@@ -76,19 +78,18 @@ class LoadingData(object):
         self.family = fieldsDict[field_name]['metadata']['family']
         self.degree = fieldsDict[field_name]['metadata']['degree']
         self.dim = fieldsDict[field_name]['metadata']['dim']
-        
         self.functionspace = df.VectorFunctionSpace(self.mesh, self.family,
                                                     self.degree, self.dim)
 
-        name = str([item[0] for item in fieldsDict[field_name]['data'].items() if item[1]==t][0])
-        
+        name = str([item[0] for item in fieldsDict[field_name]['data'].items()
+                    if item[1] == t][0])
+
         f_loaded = df.Function(self.functionspace)
-        
+
         # This line causes segmentation fault.
         self.h5file.read(f_loaded, name)
-        
+
         return f_loaded
 
     def close(self):
         self.h5file.close()
-
