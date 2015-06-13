ce2993a2a5ce3945b8bea62fd0792538ebd46b25
mb4e10 <mb4e10@soton.ac.uk>
2015-06-13 15:14:39 +0100

    Tidy up mpi testing scripts.

--- src/mpi_check.py
+++ src/mpi_check.py
@@ -1,13 +1,10 @@
-import json
-from collections import OrderedDict
-
 import numpy as np
 import dolfin as df
 
 from savingdata import Create, Read
 
 mesh = df.UnitSquareMesh(10, 10)
-filename = 'file_vector'
+filename = 'file_mpi'
 functionspace = df.VectorFunctionSpace(mesh, 'CG', 1, 3)
 f = df.Function(functionspace)
 t_array = np.linspace(0, 1, 5)
@@ -15,7 +12,7 @@ t_array = np.linspace(0, 1, 5)
 # Load data.
 ld = Read(filename)
 for t in t_array:
-    f.assign(df.Constant((1+t, 2, 3)))
+    f.assign(df.Constant((1 + t, 2, 3)))
     f_loaded = ld.load_field('f', t)
 
     assert df.assemble(f[0]*df.dx) == df.assemble(f_loaded[0]*df.dx)
