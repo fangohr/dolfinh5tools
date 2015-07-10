6f59007a5171efd7dec1d8688f11988d5350aee4
mb4e10 <mb4e10@soton.ac.uk>
2015-07-10 12:15:30 +0100

    Update mpi testing scripts.

--- src/mpi_check.py
+++ src/mpi_check.py
@@ -15,5 +15,8 @@ for t in t_array:
     f.assign(df.Constant((1 + t, 2, 3)))
     f_loaded = ld.load_field('f', t)
 
+    print df.assemble(f[0]*df.dx)
+    print df.assemble(f_loaded[0]*df.dx)
     assert df.assemble(f[0]*df.dx) == df.assemble(f_loaded[0]*df.dx)
+    print '------------'
 ld.close()
