81127470d2802a604a8abad63fe16f71d6409601
Hans Fangohr osiris <fangohr@soton.ac.uk>
2015-10-02 14:10:38 +0100

    Refactor interface design (with Marijan)

---
+++ src/test_dolfin5htools_usage.py
@@ -0,0 +1,49 @@
+import pytest
+
+def test_field2_has_more_timesteps_than_field1():
+    pass
+
+
+@pytest.skip
+def test_reading():
+
+    h5file = h5tools.open(filename)
+    for field in h5file.fields:
+        for t in h5file.times(field):
+            _ = h5file.read(field, t)
+
+
+def test_reading_fixed_field():
+
+    h5file = h5tools.open(filename, field='m')
+    for t in h5file.times:
+        _ = h5file.read(t)
+
+def test_writing():
+
+    h5file = h5tools.open(filename, 'w')
+    for t in simulation_time_steps:
+        # compute field1
+        # compute field2
+        for field in [field1, field2]:
+            h5file.write(field, time, fieldname)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
